66093105323e1f69c98e20511c512e5e5c5200cc
Phan Nhat Duy <duyphan2398@gmail.com>
2020-12-23 14:46:36 +0700

    ref: production api link

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,12 +4,12 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"https://www.api.tnguyenofficial.com"',
-  USER_API_ENDPOINT: '"https://www.api.tnguyenofficial.com"',
-  WEB_ENDPOINT: '"https://www.api.tnguyenofficial.com"',
+  // API_ENDPOINT: '"https://www.api.tnguyenofficial.com"',
+  // USER_API_ENDPOINT: '"https://www.api.tnguyenofficial.com"',
+  // WEB_ENDPOINT: '"https://www.api.tnguyenofficial.com"',
 
-  // API_ENDPOINT: '"http://personal_blog_api.test"',
-  // USER_API_ENDPOINT: '"http://personal_blog_api.test"',
-  // WEB_ENDPOINT: '"http://personal_blog_api.test"',
+  API_ENDPOINT: '"http://personal_blog_api.test/api/cms"',
+  USER_API_ENDPOINT: '"http://personal_blog_api.test/api/cms"',
+  WEB_ENDPOINT: '"http://personal_blog_api.test/api/cms"',
   CLIENT_ENV: '"development"'
 })
