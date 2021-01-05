b8d62127fc2e22f82323095ebd0066fb2c61b4be
Phan Nhat Duy <duyphan2398@gmail.com>
2021-01-05 14:09:08 +0700

    ref

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ENDPOINT: '"https://www.api.tnguyenofficial.com/api/cms"',
-  USER_API_ENDPOINT: '"https://www.api.tnguyenofficial.com/api/cms"',
-  WEB_ENDPOINT: '"https://www.api.tnguyenofficial.com/api/cms"',
+  API_ENDPOINT: '"https://www.atnguyenofficial.com/api/cms"',
+  USER_API_ENDPOINT: '"https://www.tnguyenofficial.com/api/cms"',
+  WEB_ENDPOINT: '"https://www.tnguyenofficial.com/api/cms"',
 }
