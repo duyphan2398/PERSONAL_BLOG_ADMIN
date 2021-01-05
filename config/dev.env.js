72a1c8d8d763fdd9ada6936b2f268e8217da0d70
Phan Nhat Duy <duyphan2398@gmail.com>
2021-01-05 23:40:34 +0700

    feat: change route to server

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"https://www.tnguyenofficial.com/api/cms"',
-  USER_API_ENDPOINT: '"https://www.tnguyenofficial.com/api/cms"',
-  WEB_ENDPOINT: '"https://www.tnguyenofficial.com/api/cms"',
+  API_ENDPOINT: '"https://tnguyenofficial-blog.herokuapp.com/api/cms"',
+  USER_API_ENDPOINT: '"https://tnguyenofficial-blog.herokuapp.com/api/cms"',
+  WEB_ENDPOINT: '"https://tnguyenofficial-blog.herokuapp.com/api/cms"',
 
   // API_ENDPOINT: '"http://personal_blog_api.test/api/cms"',
   // USER_API_ENDPOINT: '"http://personal_blog_api.test/api/cms"',
